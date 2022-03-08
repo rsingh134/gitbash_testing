e3e75640e84d034b2b24ca543401841a2f2e1da1
Singh <rajat.g.singh@accenture.com>
2022-03-08 15:53:02 +0530

    first commit

--- main.tf
+++ main.tf
@@ -4,3 +4,22 @@
      region      = "europe-west4"
      zone        = "europe-west4-a"
 }
+
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
