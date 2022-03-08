ca4017e2b639f21dc3c329e4b8e53f78dc6b7020
Singh <rajat.g.singh@accenture.com>
2022-03-08 15:59:19 +0530

    second change

--- main.tf
+++ main.tf
@@ -5,21 +5,3 @@
      zone        = "europe-west4-a"
 }
 
-resource "google_compute_instance" "default" {
-  name         = "test"
-  machine_type = "e2-micro"
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
-
-  network_interface {
-    network = "default"
-
-    access_config {
-      // Ephemeral IP
-    }
-  }
-}
